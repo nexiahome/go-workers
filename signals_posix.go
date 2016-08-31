db1c756d6038459d2548895fb01eab74f58f59e6
jgeiger <joey.geiger@irco.com>
2016-08-31 12:33:24 -0600

    Don't Quit on USR1

--- signals_posix.go
+++ signals_posix.go
@@ -10,11 +10,11 @@ import (
 
 func handleSignals() {
 	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, syscall.SIGUSR1, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
 
 	for sig := range signals {
 		switch sig {
-		case syscall.SIGINT, syscall.SIGUSR1, syscall.SIGTERM:
+		case syscall.SIGINT, syscall.SIGTERM:
 			Quit()
 		}
 	}
